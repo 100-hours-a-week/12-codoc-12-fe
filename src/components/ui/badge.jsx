8700880b6d7b5bf6317ca8c23a02161226655667
gnsdp <rladpgns99@gmail.com>
2026-01-26 17:27:02 +0900

    [feat] #9 shadcn ui 컴포넌트 추가

---
+++ src/components/ui/badge.jsx
@@ -0,0 +1,25 @@
+import { cva } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
+
+const badgeVariants = cva(
+  'inline-flex items-center rounded-full border border-transparent px-2.5 py-0.5 text-xs font-semibold transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground',
+        secondary: 'bg-secondary text-secondary-foreground',
+        outline: 'border-border text-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+function Badge({ className, variant, ...props }) {
+  return <div className={cn(badgeVariants({ variant, className }))} {...props} />
+}
+
+export { Badge, badgeVariants }
