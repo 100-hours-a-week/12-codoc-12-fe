f168b3a126ebf3358ec5d36d956ff72cb7be1da4
gnsdp <rladpgns99@gmail.com>
2026-01-28 15:32:25 +0900

    [feat] 챗봇 페이지 라우트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import MyPage from '@/pages/MyPage'
 import Onboarding from '@/pages/Onboarding'
+import Chatbot from '@/pages/Chatbot'
 import Quiz from '@/pages/Quiz'
 import ProblemDetail from '@/pages/ProblemDetail'
 import Problems from '@/pages/Problems'
@@ -32,6 +33,7 @@ function App() {
         <Route element={<Home />} index />
         <Route element={<Problems />} path="problems" />
         <Route element={<ProblemDetail />} path="problems/:problemId" />
+        <Route element={<Chatbot />} path="problems/:problemId/chatbot" />
         <Route element={<Quiz />} path="problems/:problemId/quiz" />
         <Route element={<MyPage />} path="my" />
       </Route>
