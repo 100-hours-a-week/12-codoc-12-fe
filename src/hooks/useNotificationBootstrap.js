ebb4632dfdbf57bebea233bd910b99445b80b454
gnsdp <rladpgns99@gmail.com>
2026-03-19 10:03:23 +0900

    [feat] foreground 수신 알림 추가

--- src/hooks/useNotificationBootstrap.js
+++ src/hooks/useNotificationBootstrap.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 
+import { subscribeToForegroundMessages } from '@/lib/firebaseMessaging'
 import { useNotificationStore } from '@/stores/useNotificationStore'
 
 export const useNotificationBootstrap = () => {
   const refreshUnreadStatus = useNotificationStore((state) => state.refreshUnreadStatus)
+  const setUnreadStatus = useNotificationStore((state) => state.setUnreadStatus)
 
   useEffect(() => {
     refreshUnreadStatus()
@@ -28,4 +30,33 @@ export const useNotificationBootstrap = () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange)
     }
   }, [refreshUnreadStatus])
+
+  useEffect(() => {
+    let isMounted = true
+    let unsubscribe = () => {}
+
+    const registerForegroundMessageListener = async () => {
+      try {
+        const resolvedUnsubscribe = await subscribeToForegroundMessages(() => {
+          setUnreadStatus(true)
+        })
+
+        if (!isMounted) {
+          resolvedUnsubscribe()
+          return
+        }
+
+        unsubscribe = resolvedUnsubscribe
+      } catch {
+        unsubscribe = () => {}
+      }
+    }
+
+    registerForegroundMessageListener()
+
+    return () => {
+      isMounted = false
+      unsubscribe()
+    }
+  }, [setUnreadStatus])
 }
